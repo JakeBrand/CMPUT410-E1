88e95b5b18eefafd27f1166dac5df413794c71c7
JakeBrand <jabrand@ualberta.ca>
2015-01-13 09:17:11 -0700

    commented out main and added divide by zero check

--- name-family.py
+++ name-family.py
@@ -19,10 +19,15 @@ class Student():
         sum = 0
         for val in self.course_marks_dict.values():
             sum = sum + val
-        avg = sum / len(self.course_marks_dict)
-        return avg
-
+        if(len(self.course_marks_dict) > 0):
+            avg = sum / len(self.course_marks_dict)
+            return avg
+        else:
+            return 0
+'''
 if __name__ == "__main__":
     student = Student("Brand", "Jake")
-    student.test()
-    print("avg", student.average())
+    student.addCourseMark("CMPUT 101", 4.0)
+    student.addCourseMark("CMPUT 123", 3.8)
+    print("student average:", student.average())
+'''
